8aafe6149e0218f5b9e37fd06472493cc984cff5
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-11-24 00:15:17 -0300

    navbar

--- src/components/NavBar/NavBar.js
+++ src/components/NavBar/NavBar.js
@@ -4,17 +4,15 @@ function NavBar () {
     return(
 
         <nav>
-            <a>
-                <h2>logo</h2>
-            </a>
+            <h2><a href=''>logo</a></h2>
 
-            <div>
-                <ul>
+            <div >
+                <ul id='navbar'>
                     <li><a href=''>Home</a></li>
-                    <li><a href=''>Home</a></li>
-                    <li><a href=''>Home</a></li>
-                    <li><a href=''>Home</a></li>
-                    <li><a href=''>Home</a></li>
+                    <li><a href=''>Shop</a></li>
+                    <li><a href=''>Blog</a></li>
+                    <li><a href=''>About</a></li>
+                    <li><a href=''>Contact</a></li>
                 </ul>
             </div>
         </nav>
