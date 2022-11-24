1ba023c3d08b2024f77ae45b37a1c75eb1671d66
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-11-23 23:50:54 -0300

    first commit

---
+++ src/components/NavBar/NavBar.js
@@ -0,0 +1,24 @@
+import '../NavBar/NavBar.css'
+
+function NavBar () {
+    return(
+
+        <nav>
+            <a>
+                <h2>logo</h2>
+            </a>
+
+            <div>
+                <ul>
+                    <li><a href=''>Home</a></li>
+                    <li><a href=''>Home</a></li>
+                    <li><a href=''>Home</a></li>
+                    <li><a href=''>Home</a></li>
+                    <li><a href=''>Home</a></li>
+                </ul>
+            </div>
+        </nav>
+    )
+}
+
+export default NavBar
