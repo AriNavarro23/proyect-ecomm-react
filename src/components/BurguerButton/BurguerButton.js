88f4ed9ad6fbe4e23acf76ccc5dd146e700c909f
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-11-27 23:59:25 -0300

    burguer

---
+++ src/components/BurguerButton/BurguerButton.js
@@ -0,0 +1,14 @@
+import React from "react";
+import '../BurguerButton/BurguerButton.css'
+
+
+
+function BurguerButton() {
+    return (
+        <div>
+            <a className="hamburger" href="#"><span></span></a>
+        </div>
+    )
+}
+
+export default BurguerButton
