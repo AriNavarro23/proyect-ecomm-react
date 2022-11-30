6485ba2356d46aa1220540543f793ab0ab19970d
Ariel Navarro <navarro.a.ariel@hotmail.com>
2022-11-30 18:36:26 -0300

    mejoras

---
+++ src/components/CartWidget/CartWidget.jsx
@@ -0,0 +1,18 @@
+import './CartWidget.css'
+import { useContext } from 'react';
+import CartContext from '../../notification/CartContext';
+import { Link } from 'react-router-dom';
+
+const CartWidget = () => {
+
+    const { getQuantity } = useContext(CartContext)
+
+    return(
+        <Link to='/cart' className="CartWidget">
+            <img src="/images/cart.svg" alt='cart' className='CartImg'/>
+            { getQuantity() }
+        </Link>
+    );
+}
+
+export default CartWidget
---
+++ src/components/NavBar/NavBar.jsx
@@ -0,0 +1,41 @@
+import './NavBar.css'
+import CartWidget from '../CartWidget/CartWidget'
+import { Link, NavLink } from 'react-router-dom'
+import { useState, useEffect } from 'react'
+import { getCategories } from '../../asyncmock'
+//import { firestoreDb } from '../../services/firebase'
+//import { getDocs, collection, orderBy, query } from 'firebase/firestore'
+
+const NavBar = () => {
+    const [categories, setCategories] = useState([])
+
+    useEffect(() => {
+        getCategories().then(categories => {
+            setCategories(categories)
+        })
+
+
+        /* getDocs(query(collection(firestoreDb, 'categories'), orderBy("order", "asc"))).then(response => {
+            const categories = response.docs.map(doc => {
+                return { id: doc.id, ...doc.data() }
+            })
+            setCategories(categories) 
+        }) */
+    }, [])
+
+    return (
+        <nav className="NavBar" >
+            <Link to='/'>
+                <h3>Ecommerce</h3>
+            </Link>
+            <div className="Categories">
+                {categories.map(cat => <NavLink key={cat.id} to={`/category/${cat.id}`}
+                    className={({ isActive }) => isActive ? 'ActiveOption' : 'Option'}
+                >{cat.description}</NavLink>)}
+            </div>
+            <CartWidget />
+        </nav>
+    )
+}
+
+export default NavBar
